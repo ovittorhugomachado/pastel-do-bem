45d0a8d630a038e44ad2f0036a5207d505367ac7
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-12-04 19:17:56 -0300

    rotas criadas

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,4 @@
+
 import { IoBagHandle } from "react-icons/io5"
 import { DonationButton } from "./donation-button"
 
@@ -9,7 +10,6 @@ export const Main = () => {
                     <h2 className="text-5xl md:text-6xl text-[#D92525] mb-6 font-hand">
                         Uma Doçura que Salva Vidas
                     </h2>
-
                     <div className="space-y-6 text-xl md:text-xl text-gray-700 leading-relaxed font-sans font-light">
                         <p className="text-start">
                             O <span className="font-bold text-[#D92525]">Pastel do Bem</span> é um movimento de solidariedade que faz diferença na vida de muitas crianças.
@@ -20,76 +20,55 @@ export const Main = () => {
                             Sua contribuição gera impacto real e leva esperança a quem mais precisa.
                         </p>
                     </div>
-
                     <div className="pt-8">
                         <div className="w-24 h-1 bg-[#F2C94C] mx-auto rounded-full"></div>
                     </div>
                 </div>
             </section>
-
-            {/* SEÇÃO DE DOAÇÃO */}
             <section id="donate" className="py-10 bg-[#FFFDF5] relative">
                 <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none opacity-10">
                     <div className="absolute top-10 left-10 w-32 h-32 bg-[#F2C94C] rounded-full blur-3xl"></div>
                     <div className="absolute bottom-10 right-10 w-48 h-48 bg-[#D92525] rounded-full blur-3xl"></div>
                 </div>
-
                 <div className="container relative z-10 text-center">
-                    <h2 className="text-5xl md:text-6xl text-[#D92525] mb-4 font-hand">
-                        Faça sua Doação
-                    </h2>
-                    <p className="text-xl text-gray-700 mb-12 max-w-2xl mx-auto">
-                        Escolha um valor e ajude a transformar o futuro de muitas crianças.<br /> Todo valor conta!
-                    </p>
-
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto mb-16">
-                        <DonationButton value={10} link="https://pagamento.pagbank.com.br/pagamento?code=b209ff36-dbbd-4888-8d90-241856850038&t=0d87131f-11bc-409f-a051-32e642b30ff2" />
-                        <DonationButton value={20} link="https://pagamento.pagbank.com.br/pagamento?code=6ddfe018-1b85-4a92-8e16-c88da3052ed8&t=b355282d-0b22-4195-8a3b-831707bf0a48" />
-                        <DonationButton value={30} link="https://pagamento.pagbank.com.br/pagamento?code=2bb4bbbf-d2bc-409e-8d67-e1fb7a371ee8&t=28134408-a0b9-4214-a133-c64cd9069e7d" />
-                        <DonationButton value={50} link="https://pagamento.pagbank.com.br/pagamento?code=ee9f37d6-cf00-4340-8b21-10ba78932882&t=f537369c-96d7-4fd9-a8d8-46671aa69c56" />
-                        <DonationButton value={100} link="https://pagamento.pagbank.com.br/pagamento?code=fa52fb81-7660-4c0a-8eaf-c0ae578b1096&t=d0db2f5e-ddca-4e4f-86ed-f550825f1d1d" />
-                    </div>
-
-                    <div className="max-w-4xl mx-auto mt-20">
+                    <div className="max-w-4xl mx-auto mb-20">
                         <div className="overflow-hidden border-none shadow-xl bg-white relative rounded-xl">
-
-                            {/* CARD 1 — PASTEL VIRTUAL */}
                             <div className="p-0">
-                                {/* ★ Barra de gradiente CORRETA */}
                                 <div className="w-full h-2 rounded-t-2xl bg-linear-to-r from-[#D92525] via-[#F2C94C] to-[#27AE60]"></div>
-
                                 <div className="bg-white shadow-2xl rounded-2xl">
                                     <div className="p-8 md:p-12 flex flex-col justify-center text-center">
                                         <h3 className="text-3xl md:text-4xl text-[#D92525] mb-4 font-hand">
-                                            Você também pode comprar o Pastel Virtual
+                                            Você pode comprar o Pastel Virtual
                                         </h3>
-
                                         <p className="text-gray-600 mb-6 font-sans text-lg">
-                                            Transforme o futuro com o Pastel Virtual, uma <span className="font-bold">doação simbólica de R$ 12,00 </span> que faz toda a diferença.
+                                            Transforme o futuro com o Pastel Virtual, uma <span className="font-bold">doação simbólica </span> que faz toda a diferença.
                                         </p>
-
-                                        <div className="flex justify-center items-center gap-4 mb-8">
+                                        {/* <div className="flex justify-center items-center gap-4 mb-8">
                                             <span className="text-3xl font-bold text-gray-800 font-sans">R$ 12,00</span>
+                                        </div> */}
+                                        <div className="flex justify-center gap-3 flex-wrap">
+                                            <a
+                                                target="_blank"
+                                                href="https://pagamento.pagbank.com.br/pagamento?code=92bdfef2-4995-4a77-b505-d99d054b3ff7&t=09f9e546-8d89-4784-a75e-fd3f89574dc8"
+                                                className="flex max-w-120 items-center justify-center w-full bg-[#27AE60] hover:bg-[#219150] text-white font-hand text-xl md:text-2xl py-2 rounded-2xl shadow-lg hover:shadow-xl transition-all transform hover:-translate-y-1"
+                                            >
+                                                <IoBagHandle className="mr-2 h-6 w-6" />1 unidade R$ 12,00
+                                            </a>
+                                            <a
+                                                target="_blank"
+                                                href="https://pagamento.pagbank.com.br/pagamento?code=92bdfef2-4995-4a77-b505-d99d054b3ff7&t=09f9e546-8d89-4784-a75e-fd3f89574dc8"
+                                                className="flex max-w-120 items-center justify-center w-full bg-[#27AE60] hover:bg-[#219150] text-white font-hand text-xl md:text-2xl py-2 rounded-2xl shadow-lg hover:shadow-xl transition-all transform hover:-translate-y-1"
+                                            >
+                                               <IoBagHandle className="mr-2 h-6 w-6" />6 unidades R$ 59,90
+                                            </a> 
                                         </div>
-
-                                        <a
-                                            target="_blank"
-                                            href="https://pagamento.pagbank.com.br/pagamento?code=92bdfef2-4995-4a77-b505-d99d054b3ff7&t=09f9e546-8d89-4784-a75e-fd3f89574dc8"
-                                            className="h-12 flex justify-center items-center text-[#D92525] py-6 px-8 text-2xl font-hand font-bold border-2 border-[#d8d8d8] hover:bg-[#D40924] transition-all duration-300 rounded-2xl shadow-hand hover:text-white transform hover:-translate-y-1"
-                                        >
-                                            Comprar
-                                        </a>
-
                                         <span className="text-sm mt-6">
                                             *A compra do pastel virtual é um valor simbólico que não representa a compra do pastel
                                         </span>
                                     </div>
                                 </div>
                             </div>
-
-                            {/* CARD 2 — CAIXA COM 6 PASTÉIS */}
-                            <div className="p-0 mt-12">
-                                {/* ★ Agora a segunda caixa TAMBÉM tem sua própria barra */}
+                            {/* <div className="p-0 mt-12">
                                 <div className="w-full h-2 rounded-t-2xl bg-linear-to-r from-[#D92525] via-[#F2C94C] to-[#27AE60]"></div>
 
                                 <div className="grid md:grid-cols-2">
@@ -103,7 +82,6 @@ export const Main = () => {
                                             Edição Especial
                                         </div>
                                     </div>
-
                                     <div className="p-8 md:p-12 flex flex-col justify-center text-center md:text-left">
                                         <h3 className="text-3xl md:text-4xl text-[#D92525] mb-4 font-hand">
                                             Leve o Sabor para Casa
@@ -132,11 +110,22 @@ export const Main = () => {
                                         </p>
                                     </div>
                                 </div>
-                            </div>
-
+                            </div> */}
                         </div>
                     </div>
-
+                    <h2 className="text-5xl md:text-6xl text-[#D92525] mb-4 font-hand">
+                        Faça sua Doação
+                    </h2>
+                    <p className="text-xl text-gray-700 mb-12 max-w-2xl mx-auto">
+                        Escolha um valor e ajude a transformar o futuro de muitas crianças.<br /> Todo valor conta!
+                    </p>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto mb-16">
+                        <DonationButton value={10} link="https://pagamento.pagbank.com.br/pagamento?code=b209ff36-dbbd-4888-8d90-241856850038&t=0d87131f-11bc-409f-a051-32e642b30ff2" />
+                        <DonationButton value={20} link="https://pagamento.pagbank.com.br/pagamento?code=6ddfe018-1b85-4a92-8e16-c88da3052ed8&t=b355282d-0b22-4195-8a3b-831707bf0a48" />
+                        <DonationButton value={30} link="https://pagamento.pagbank.com.br/pagamento?code=2bb4bbbf-d2bc-409e-8d67-e1fb7a371ee8&t=28134408-a0b9-4214-a133-c64cd9069e7d" />
+                        <DonationButton value={50} link="https://pagamento.pagbank.com.br/pagamento?code=ee9f37d6-cf00-4340-8b21-10ba78932882&t=f537369c-96d7-4fd9-a8d8-46671aa69c56" />
+                        <DonationButton value={100} link="https://pagamento.pagbank.com.br/pagamento?code=fa52fb81-7660-4c0a-8eaf-c0ae578b1096&t=d0db2f5e-ddca-4e4f-86ed-f550825f1d1d" />
+                    </div>
                 </div>
             </section>
         </main>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/pasteldobem" element={<App />} />
+        <Route path="*" element={<h1 className='mx-auto'>Página não encontrada - 404</h1>} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
