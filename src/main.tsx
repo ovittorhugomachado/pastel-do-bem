54122b305eef5fe32d773b3c9d87d8711e5b932d
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-12-04 19:22:12 -0300

    sem rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/pasteldobem" element={<App />} />
-        <Route path="*" element={<h1 className='mx-auto'>Página não encontrada - 404</h1>} />
-      </Routes>
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
